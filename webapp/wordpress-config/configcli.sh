b901f41a9444d81f98451a0c81d009d440d7bdfa
Pawel Pazderski <pawelpazderski@hotmail.com>
2025-07-23 16:24:46 +0200

    fix

--- webapp/wordpress-config/configcli.sh
+++ webapp/wordpress-config/configcli.sh
@@ -3,8 +3,13 @@
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
   && chmod +x wp-cli.phar \
   && mv wp-cli.phar /usr/local/bin/wp
+# Wait for DB to be ready
+until wp db check --allow-root; do
+  echo "Waiting for database connection..."
+  sleep 5
+done
 # Finish installation of wordpress using cli
-su deployuser -c "
+su www-data -c "
 cd /var/www/html &&
 exec wp core install \
   --url=${WORDPRESS_URL} \
