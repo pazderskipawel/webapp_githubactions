613d824c13d0fa79087b56e6c32c927b80908164
Pawel Pazderski <pawelpazderski@hotmail.com>
2025-07-23 21:39:25 +0200

    fix????

--- webapp/wordpress-config/allowhttp.sh
+++ webapp/wordpress-config/allowhttp.sh
@@ -2,5 +2,5 @@
 su deployuser
 cd /var/www/html
 # Update WordPress site URL and home URL to allow HTTP access
-exec wp option update siteurl http://localhost:8080
-exec wp option update home http://localhost:8080
+wp option update siteurl http://localhost:8080
+wp option update home http://localhost:8080
