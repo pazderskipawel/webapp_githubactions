996296f5a9f6ae91173f86709c60c68a14ceeb1b
Pawel Pazderski <pawelpazderski@hotmail.com>
2025-07-23 16:01:00 +0200

    modified script

--- webapp/wordpress-config/configcli.sh
+++ webapp/wordpress-config/configcli.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
+# install wp-cl
+curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar \
+  && chmod +x wp-cli.phar \
+  && mv wp-cli.phar /usr/local/bin/wp
+# Finish installation of wordpress using cli
 su deployuser -c "
 cd /var/www/html &&
-exec wp core is-installed || wp core install \
+exec wp core install \
   --url=${WORDPRESS_URL} \
   --title="${WORDPRESS_SITE_TITLE}" \
   --admin_user="${WORDPRESS_ADMIN_USER}" \
