4b280ebf834a0d4b5b13aad6d2b8df60c2c7fe75
Pawel Pazderski <pawelpazderski@hotmail.com>
2025-07-23 17:44:44 +0200

    fix2

--- webapp/wordpress-config/configcli.sh
+++ webapp/wordpress-config/configcli.sh
@@ -16,5 +16,5 @@ exec wp core install \
   --title="${WORDPRESS_SITE_TITLE}" \
   --admin_user="${WORDPRESS_ADMIN_USER}" \
   --admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
-  --admin_email="${WORDPRESS_ADMIN_EMAIL}" &&
+  --admin_email="${WORDPRESS_ADMIN_EMAIL}"
 "
