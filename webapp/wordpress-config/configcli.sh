9596c12c9b6d9dc12a496c30e732e81ccfebfc3d
Pawel Pazderski <pawelpazderski@hotmail.com>
2025-07-23 15:13:51 +0200

    added step in CICD setting up container after it starts

--- webapp/wordpress-config/configcli.sh
+++ webapp/wordpress-config/configcli.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
-chown www-data:www-data /var/www/html/wp-config.php
-
-adduser --disabled-password --gecos '' deployuser
-
 su deployuser -c "
-wp core is-installed || wp core install \
+cd /var/www/html &&
+exec wp core is-installed || wp core install \
   --url=${WORDPRESS_URL} \
   --title="${WORDPRESS_SITE_TITLE}" \
   --admin_user="${WORDPRESS_ADMIN_USER}" \
   --admin_password="${WORDPRESS_ADMIN_PASSWORD}" \
   --admin_email="${WORDPRESS_ADMIN_EMAIL}"
 "
-exec apache2-foreground
